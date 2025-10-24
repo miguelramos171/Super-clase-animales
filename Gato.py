11fdaa382662077094cf529f422a5500cea313f4
miguelramos171 <miramoss@unal.edu.co>
2025-10-23 20:47:48 -0500

    Add Gato class that extends Animal

--- Gato.py
+++ Gato.py
@@ -1,1 +1,12 @@
+from animal import Animal
 
+class Gato(Animal):
+
+    def __init__(self, nombre):
+        super().__init__(nombre, tipo_movimiento = "correr")
+
+    def hablar(self):
+        print(f"{self.nombre} dice: ¡Miau miau!")
+
+    def jugar(self):
+        print(f"{self.nombre} está corriendo tras un ratón de juguete")
