7e1049922a8141d5f23c8b554a291d5948d95b83
miguelramos171 <miramoss@unal.edu.co>
2025-10-23 20:50:24 -0500

    Add main function to manage animal interactions

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from Perro import Perro
+from Gato import Gato
+from Pez import Pez
 
+def main():
+    animales = [Perro("Mateo"), Gato("Michi"), Pez("Pipe")]
+    
+    hamster= Perro("pepo")
+    hamster.jugar()
+    hamster = Gato("pepo")
+    hamster.jugar()
+
+    for animal in animales:
+        animal.hablar()
+        animal.moverse()
+        animal.jugar()
+        print()
+
+if __name__ == "__main__":
+    main()
