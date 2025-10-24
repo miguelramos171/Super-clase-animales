1f32bf77698789601b1c261c5af28d03c6961629
miguelramos171 <miramoss@unal.edu.co>
2025-10-23 20:46:17 -0500

    ...

--- Perro.py
+++ Perro.py
@@ -1,1 +1,13 @@
+from animales import Animales 
+class Perro(Animal):
+
+    def __init__(self, nombre):
+        super().__init__(nombre, tipo_movimiento = "correr")
+
+
+    def hablar(self):
+        print(f"\n{self.nombre} dice Guauu!!!")
+
+    def jugar(self):
+        print(f"{self.nombre} está corriendo tras una pelota")
 
