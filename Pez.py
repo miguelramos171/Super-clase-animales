e8cc1d15c0af966639ae65b0774b57e765c20f4f
miguelramos171 <miramoss@unal.edu.co>
2025-10-23 20:48:38 -0500

    Add Pez class inheriting from Animal

--- Pez.py
+++ Pez.py
@@ -1,1 +1,13 @@
+from animal import Animal
+
+class Pez(Animal):
+
+    def __init__(self, nombre):
+        super().__init__(nombre, tipo_movimiento = "nadar")
+
+    def hablar(self):
+        print(f"{self.nombre} empezó a liberar burbujas como sonido")
+
+    def jugar(self):
+        print(f"{self.nombre} está persiguiendo un renacuajo")
 
